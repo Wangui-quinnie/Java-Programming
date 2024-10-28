a6761fdba9ea9e22392b75448ff349d0204ce6e8
Wangui-quinnie <w.wangui@ymail.com>
2024-10-28 13:28:54 +0300

    arithmetic operations

---
+++ arithmeticOp.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void Main(string [] args) {
+        int number1 = 20;
+        int number2 = 5;
+        
+        //addition
+        system.out.println(number1 + number2); //25
+
+        //subtraction
+        system.out.println(number1 - number2); //15
+
+        //division
+        system.out.println(number1 / number2); //4
+
+        //multiplication
+        system.out.println(number1 * number2); //100
+
+        //remainder/modulo
+        system.out.println(number1 % number2); //0
+
+    }
+}
