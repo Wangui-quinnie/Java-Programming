d8de1e0cbb6720d091d7d5c384badbce2e9664b4
Wangui-quinnie <125828749+Wangui-quinnie@users.noreply.github.com>
2024-11-02 17:57:25 +0300

    variables and short comments

---
+++ variables.java
@@ -0,0 +1,16 @@
+public class Main { //This is a comment
+    public static void main(String[] args) {
+        //create variables
+        int length = 10;
+        int width = 4;
+        int area;
+
+        //caculate area of a rectangle
+        area = length * width;
+
+        //print variables
+        System.out.println("Length is " + length);
+        System.out.println("Width is " + width);
+        System.out.println("Rectangle area is " + area);
+    }
+}
