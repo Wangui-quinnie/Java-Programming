ef31ee9407a7e3a67267a687e6a425edb54a69d5
Wangui-quinnie <w.wangui@ymail.com>
2024-11-02 21:31:30 +0300

    data types

---
+++ dataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //primitive data types in java
+        byte myNumber = 100;
+        short num1 = 5000;
+        long num2 = 10000000000L;
+        double myDouble = 14.55d; // double has a precision of 16 digits
+        int myNum = 500000; // Integer (whole number)
+        float myFloatNum = 5.99f; // Floating point number witha precision of 6/7 digits
+        char myLetter = 'D'; // Character
+        boolean iAmWinnie = true; // Boolean
+        String myText = "Hello"; // String
+
+
+        //print
+        System.out.println(myNumber);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(myDouble);
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(iAmWinnie);
+        System.out.println(myText);
+    }
+}
