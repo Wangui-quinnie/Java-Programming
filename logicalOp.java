7ed4e252a73c306808c63e341c0b155108665a3a
Wangui-quinnie <w.wangui@ymail.com>
2024-11-03 11:32:13 +0300

    Logical operators

---
+++ logicalOp.java
@@ -0,0 +1,8 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x<=5 && x<10); //true
+        System.out.println(x<6 || x<4); //true
+        System.out.println!(x<5 && x<10);//true
+    }
+}
